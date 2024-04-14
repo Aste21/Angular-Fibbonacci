70f3e11e0f41da4a12c218d2c640dcd99bc61a95
Wiktor Kopczyński <kopczynski.wkt@gmail.com>
2024-04-14 20:46:44 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  currentFib: string;
+
+  constructor() {
+    this.currentFib = '';
+  }
+
+  calculateFib(whichOne: HTMLInputElement):boolean{
+    const n = parseInt(whichOne.value);
+
+    if (isNaN(n)) {
+        this.currentFib = "Input is not a number";
+    } else if (!Number.isInteger(n)) {
+      this.currentFib = "Input is not an integer";
+    } else if (n <= 0) {
+      this.currentFib = 'The Fibonacci number is: 0';
+    } else if (n === 1) {
+      this.currentFib = 'The Fibonacci number is: 1';
+    } else {
+        let prev = 0;
+        let current = 1;
+        for (let i = 2; i <= n; i++) {
+            const temp = current;
+            current += prev;
+            prev = temp;
+        }
+        this.currentFib = "The Fibonacci number is:" + current.toString();
+    }
+    return false;
+  }
+ 
 }
